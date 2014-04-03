7c2d6a97934f2580afb99eb48c75d50a4176c886
Christopher Patton <chpatton013@gmail.com>
2014-04-03 13:51:30 -0700

    Problem 12

---
+++ problem12.rb
@@ -0,0 +1,10 @@
+require 'prime'
+i = 1
+count = 0
+while count <= 500 and i = i + 1
+   primes, powers = ((i * (i + 1))/2).prime_division.transpose
+   powers.map! {|x| x + 1}
+   count = powers.inject(1){|factors, item| factors * item}
+end
+puts ((i * (i + 1))/2)
+
